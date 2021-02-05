1655b6dbd077e3b38041bcf9ad6252591dcb2f2b
ponsiro <tatsuya_hashida@yahoo.co.jp>
2021-02-05 12:41:29 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
-end
+  before_action :authenticate_user!
+  before_action :devise_params_set, if: :devise_controller?
+
+
+  private
+  def devise_params_set
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  end
